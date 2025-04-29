1639c3efe5ba449f7bed13256ca24d6ba59958a7
alexplayer15 <alexplayer15@icloud.com>
2025-04-29 23:41:38 +0100

    01 add in more test coverage and better package management to get cli working

--- src/main.go
+++ src/main.go
@@ -4,23 +4,11 @@ import (
     "fmt"
     "os"
 
-    "github.com/spf13/cobra"
     "github.com/alexp/parmesan/src/commands"
 )
 
-var rootCmd = &cobra.Command{
-    Use:   "parmesan",
-    Short: "CLI tool to generate requests based off your OAS",
-}
-
 func main() {
-    if err := rootCmd.Execute(); err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-
-    rootCmd.AddCommand(commands.GenerateRequestCmd)  // Use GenerateRequestCmd from the commands package
-    if err := rootCmd.Execute(); err != nil {
+    if err := commands.RootCmd.Execute(); err != nil {
         fmt.Println(err)
         os.Exit(1)
     }
--- test/commands/generate_test.go
+++ test/commands/generate_test.go
@@ -9,14 +9,61 @@ import (
 )
 
 func TestGenerateRequestCmd(t *testing.T) {
+	//Arrange
 	rootCmd := &cobra.Command{
 		Use: "parmesan",
 	}
-
 	rootCmd.AddCommand(commands.GenerateRequestCmd)
 
+	//Assert
 	cmd, _, err := rootCmd.Find([]string{"generate-request"})
 	assert.NoError(t, err, "Error should be nil when finding the 'generate-request' command")
 	assert.NotNil(t, cmd, "'generate-request' command should be registered")
 	assert.Equal(t, "generate-request", cmd.Use, "Command name should be 'generate-request'")
+}
+
+func Test_WhenGenerateRequestIsNotGivenAnArg_ShouldFail(t *testing.T){
+	//Arrange
+	rootCmd := &cobra.Command{
+		Use: "parmesan",
+	}
+	rootCmd.AddCommand(commands.GenerateRequestCmd)
+	rootCmd.SetArgs([]string{"generate-request"})
+
+	//Act
+	err := rootCmd.Execute()
+
+	//Assert
+    assert.Error(t, err, "Command should fail if no argument is given")
+}
+
+func Test_WhenGenerateRequestIsGivenMoreThanOneArg_ShouldFail(t *testing.T){
+	//Arrange
+	rootCmd := &cobra.Command{
+		Use: "parmesan",
+	}
+	rootCmd.AddCommand(commands.GenerateRequestCmd)
+	rootCmd.SetArgs([]string{"generate-request", "file1.yaml", "file2.yaml"})
+
+	//Act
+	err := rootCmd.Execute()
+
+	//Assert
+    assert.Error(t, err, "Command should fail if no argument is given")
+}
+
+func Test_WhenGenerateRequestIsInitialised_ShouldHaveAnOutputFlag(t *testing.T){
+	//Arrange
+	rootCmd := &cobra.Command{
+		Use: "parmesan",
+	}
+	rootCmd.AddCommand(commands.GenerateRequestCmd)
+
+	//Act
+	cmd, _, err := rootCmd.Find([]string{"generate-request"})
+
+	//Assert
+	assert.NoError(t, err, "Command should be found")
+	flag := cmd.Flags().Lookup("output")
+	assert.NotNil(t, flag, "The 'output' flag should be registered")
 }
