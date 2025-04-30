45b37c36ee5f5e421646308e8f925b46bc5e4e88
alexplayer15 <alexplayer15@icloud.com>
2025-04-30 09:33:00 +0100

    keep patching up tests and add a request body to the oas

--- test/commands/generate_test.go
+++ test/commands/generate_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/alexp/parmesan/src/commands"
-	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -29,19 +28,13 @@ func Test_WhenGenerateRequestIsGivenMoreThanOneArg_ShouldFail(t *testing.T) {
 	//Assert
 	assert.Error(t, err, "Command should fail if no argument is given")
 }
+func Test_WhenGenerateRequestIsGivenValidArguments_ShouldNotError(t *testing.T) {
+	//Arrange 
+	commands.RootCmd.SetArgs([]string{"generate-request", "file1.yaml"})
 
-func Test_WhenGenerateRequestIsInitialised_ShouldHaveAnOutputFlag(t *testing.T) {
-	//Arrange
-	rootCmd := &cobra.Command{
-		Use: "parmesan",
-	}
-	rootCmd.AddCommand(commands.GenerateRequestCmd)
-
-	//Act
-	cmd, _, err := rootCmd.Find([]string{"generate-request"})
+	//Act 
+	err := commands.RootCmd.Execute()
 
 	//Assert
-	assert.NoError(t, err, "Command should be found")
-	flag := cmd.Flags().Lookup("output")
-	assert.NotNil(t, flag, "The 'output' flag should be registered")
+	assert.NoError(t, err, "Should be no error when 1 arg and correct command is entered")
 }
