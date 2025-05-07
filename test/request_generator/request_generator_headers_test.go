3fbfb8088f1deef2fb955f554ae1b2e468127e5a
AlexPlayerEasyJet15 <Alex.Player@easyjet.com>
2025-05-07 08:20:44 +0100

    12 add in tests for non-example scenarios

--- test/request_generator/request_generator_headers_test.go
+++ test/request_generator/request_generator_headers_test.go
@@ -32,3 +32,25 @@ func Test_WhenOASHasHeadersWithExamples_ShouldReturnExampleHeaders(t *testing.T)
 	assert.NoError(t, err)
 	assert.Equal(t, "1", headerMap["X-Session-ID"])
 }
+
+func Test_WhenOASHasHeadersWithoutExamples_ShouldReturnFallbackValues(t *testing.T) {
+	//Arrange
+	oas := test_data.BaseOAS()
+	param := test_builder.NewParameterBuilder().
+		WithName("X-Session-ID").
+		WithIn("header").
+		Build()
+
+	method := oas.Paths["/users"]["post"]
+	method.Parameters = append(method.Parameters, *param)
+	oas.Paths["/users"]["post"] = method
+
+	//Act
+	result, err := request_generator.GenerateHttpRequest(oas)
+
+	//Assert
+	assert.NoError(t, err)
+	headerMap, err := test_helpers.ExtractHeaders(t, result)
+	assert.NoError(t, err)
+	assert.Equal(t, "default-value", headerMap["X-Session-ID"])
+}
--- test/request_generator/request_generator_value_types_test.go
+++ test/request_generator/request_generator_value_types_test.go
@@ -28,6 +28,24 @@ func Test_WhenOASHasStringPropertyWithAnExample_ShouldReturnExampleString(t *tes
 	assert.Contains(t, result, `"name": "Alex"`)
 }
 
+func Test_WhenOASHasStringPropertyWithoutAnExample_ShouldReturnFallbackString(t *testing.T) {
+	// Arrange
+	oas := test_data.BaseOAS()
+
+	propName, propValue := test_builder.NewPropertyBuilder().
+		WithName("name").
+		WithType("string").
+		Build()
+	oas.Paths["/users"]["post"].RequestBody.Content["application/json"].Schema.Properties[propName] = propValue
+
+	//Act
+	result, err := request_generator.GenerateHttpRequest(oas)
+
+	//Assert
+	assert.NoError(t, err)
+	assert.Contains(t, result, `"name": "example value"`)
+}
+
 func Test_WhenOASHasIntegerPropertyWithAnExample_ShouldReturnExampleInteger(t *testing.T) {
 	// Arrange
 	oas := test_data.BaseOAS()
@@ -45,6 +63,24 @@ func Test_WhenOASHasIntegerPropertyWithAnExample_ShouldReturnExampleInteger(t *t
 	//Assert
 	assert.NoError(t, err)
 	assert.Contains(t, result, `"age": 25`)
+}
+
+func Test_WhenOASHasIntegerPropertyWithoutAnExample_ShouldReturnExampleZero(t *testing.T) {
+	// Arrange
+	oas := test_data.BaseOAS()
+
+	propName, propValue := test_builder.NewPropertyBuilder().
+		WithName("age").
+		WithType("integer").
+		Build()
+	oas.Paths["/users"]["post"].RequestBody.Content["application/json"].Schema.Properties[propName] = propValue
+
+	//Act
+	result, err := request_generator.GenerateHttpRequest(oas)
+
+	//Assert
+	assert.NoError(t, err)
+	assert.Contains(t, result, `"age": 0`)
 }
 
 func TestWhenOASHasPropertyOfTypeStringWithFormatDateAndAnExample_ShouldReturnDate(t *testing.T) {
@@ -66,4 +102,24 @@ func TestWhenOASHasPropertyOfTypeStringWithFormatDateAndAnExample_ShouldReturnDa
 	assert.NoError(t, err)
 	//have a think about how much spacing matters. Test is fragile as stands
 	assert.Contains(t, result, `"birthday": "2022-11-23"`)
+}
+
+func TestWhenOASHasPropertyOfTypeStringWithFormatDateAndNoExample_ShouldReturnFallbackDate(t *testing.T) {
+	// Arrange
+	oas := test_data.BaseOAS()
+
+	propName, propValue := test_builder.NewPropertyBuilder().
+		WithName("birthday").
+		WithType("string").
+		WithFormat("date").
+		Build()
+	oas.Paths["/users"]["post"].RequestBody.Content["application/json"].Schema.Properties[propName] = propValue
+
+	//Act
+	result, err := request_generator.GenerateHttpRequest(oas)
+
+	//Assert
+	assert.NoError(t, err)
+	//have a think about how much spacing matters. Test is fragile as stands
+	assert.Contains(t, result, `"birthday": "2022-01-01"`)
 }
